f30c70d3de4582c9365dbb34a1dabbef8ade4c9d
Adam Bull <adamjamesbull@googlemail.com>
2019-06-27 14:58:43 +0100

    Allow the Spacer component to manually specify height

--- src/app/components/ui/Spacer/index.tsx
+++ src/app/components/ui/Spacer/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import styles from "./Spacer.module.scss";
 
-const Spacer = () => (
-  <div className={styles.spacer} />
+interface SpacerProps {
+  height?: number;
+}
+
+const Spacer = ({ height }: SpacerProps) => (
+  <div className={styles.spacer} style={{ height: height }} />
 );
 
 export default Spacer;
