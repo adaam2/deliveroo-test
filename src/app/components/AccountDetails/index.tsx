ecc8b6fcfcda432ac00f35da2ea056318859c76b
Adam Bull <adamjamesbull@googlemail.com>
2019-06-27 17:08:57 +0100

    Adds AccountDetails component

---
+++ src/app/components/AccountDetails/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Icon, P } from '../ui';
+import styles from "./AccountDetails.module.scss";
+
+export interface AccountDetailsProps {
+  userName: string;
+}
+
+const AccountDetails: React.SFC<AccountDetailsProps> = ({ userName }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.flexItem}>
+        <Icon iconKey="user" />
+      </div>
+      <div className={styles.userName}>
+        <P marginLeft={8} fontSize={16} colour="#2e3333">
+          {userName}
+        </P>
+      </div>
+    </div>
+  );
+}
+
+export default AccountDetails;
