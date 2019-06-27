1f6969045ca5a1840e13a969a9c6fbe59ce5c54f
Adam Bull <adamjamesbull@googlemail.com>
2019-06-27 14:17:43 +0100

    Add deliveroo fonts into head

--- src/server/routes/index.tsx
+++ src/server/routes/index.tsx
@@ -26,26 +26,29 @@ router.get('/*', (req, res) => {
     } else {
       res.status(200).send(
         `<!doctype html>
-            <html lang="">
-            <head>
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta charset="utf-8" />
-                <title>Welcome to Razzle</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                ${
-                  assets.client.css
-                    ? `<link rel="stylesheet" href="${assets.client.css}">`
-                    : ''
-                }
-                ${
-                  process.env.NODE_ENV === 'production'
-                    ? `<script src="${assets.client.js}" defer></script>`
-                    : `<script src="${assets.client.js}" defer crossorigin></script>`
-                }
-            </head>
-            <body>
-                <div id="root">${markup}</div>
-            </body>
+          <html lang="">
+          <head>
+              <link rel="preload" href="//consumer-component-library.roocdn.com/13.13.0/static/fonts/IBMPlexSans-Regular.dc16f54ea33a9556b9035a9b05e96711.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
+              <link rel="preload" href="//consumer-component-library.roocdn.com/13.13.0/static/fonts/stratos-semibold.b00ca2148f03da7f28865214a97c568e.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+              <link rel="preload" href="//consumer-component-library.roocdn.com/13.13.0/static/fonts/IBMPlexSans-Bold.1920d19939b703c6610b0e6c197e3fce.woff2" as="font" type="font/woff2" crossorigin="anonymous" class="next-head">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+              <meta charset="utf-8" />
+              <title>Welcome to Razzle</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+              ${
+                assets.client.css
+                  ? `<link rel="stylesheet" href="${assets.client.css}">`
+                  : ''
+              }
+              ${
+                process.env.NODE_ENV === 'production'
+                  ? `<script src="${assets.client.js}" defer></script>`
+                  : `<script src="${assets.client.js}" defer crossorigin></script>`
+              }
+          </head>
+          <body>
+              <div id="root">${markup}</div>
+          </body>
         </html>`
       );
     }
